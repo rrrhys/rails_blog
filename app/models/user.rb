a46c81140d50d7092d11a7ba0bf45ad455152998
Rhys Williams <rrrhys@gmail.com>
2013-03-19 21:18:41 +1100

    Worked it out! remember_token method shared a name with remember_token field name (scope conflict I guess)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :is_administrator, :name, :password, :password_confirmation
   has_secure_password
   before_validation :not_administrator
-  before_save :remember_token
+  before_save :create_remember_token
   validates :is_administrator, :inclusion => {:in => [true, false]}
   validates_presence_of :email
   validates_presence_of :name
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   		self.is_administrator = 0
   	end
   end
-  def remember_token
+  def create_remember_token
   	if not self.remember_token?
   		self.remember_token = SecureRandom.urlsafe_base64
   	end
